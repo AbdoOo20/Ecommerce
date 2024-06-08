022b5cec8741bc556dbee75fc80a28f41bbdf7d3
Abdelrahman Emad Eldeen Ragab <87204675+AbdoOo20@users.noreply.github.com>
2024-06-08 14:17:03 +0300

    auth

--- Common/Authentication/auth.js
+++ Common/Authentication/auth.js
@@ -1,6 +1,4 @@
-import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, db, collection, addDoc, doc, setDoc } from '../../Database/firebase-config.js';
-
-
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, db, doc, setDoc } from '../../Database/firebase-config.js';
 
 function signUp() {
     var email = document.getElementById('email').value;
@@ -25,7 +23,7 @@ function signUp() {
                     password: password,
                     name: name,
                     id: user.uid
-                  }).then(() => {
+                }).then(() => {
                     createAccountButton.style.display = 'block';
                     loader.style.display = 'none';
                     window.location.href = '../../User/home/home.html';
@@ -46,18 +44,51 @@ function signUp() {
 }
 
 function logIn() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log('Log In Successful:', user);
-        })
-        .catch((error) => {
-            console.error('Error logging in:', error.message);
-        });
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var createAccountButton = document.querySelector('.auth-button');
+    var loader = document.querySelector('.loading-indicator');
+    if (email === "") {
+        alert("You must enter email");
+    } else if (password === "") {
+        alert("You must enter password");
+    } else {
+        createAccountButton.style.display = 'none';
+        loader.style.display = 'block';
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                createAccountButton.style.display = 'block';
+                loader.style.display = 'none';
+                window.location.href = '../../User/home/home.html';
+                email = "";
+                password = "";
+            })
+            .catch((error) => {
+                email = "";
+                password = "";
+                alert(error.message);
+                createAccountButton.style.display = 'block';
+                loader.style.display = 'none';
+            });
+    }
+
 }
 
-document.getElementById('createAccount').addEventListener('click', function () {
-    signUp();
-});
+
+
+function onLoadPage() {
+    var list = window.location.href.split('/');
+    console.log(list);
+    if (list[list.length - 1] === 'register.html') {
+        document.getElementById('createAccount').addEventListener('click', function () {
+            signUp();
+        });
+    }
+    if (list[list.length - 1] === 'login.html') {
+        document.getElementById('login').addEventListener('click', function () {
+            logIn();
+        });
+    }
+}
+
+onLoadPage();
