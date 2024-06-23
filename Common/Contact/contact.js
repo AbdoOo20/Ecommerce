e2994918c36f66088197e73c1efb9cce6289898f
AmgaadEzzat <amgaadezzat99@gmail.com>
2024-06-23 09:39:52 +0300

    modefied the wishlist page

--- Common/Contact/contact.js
+++ Common/Contact/contact.js
@@ -24,21 +24,3 @@ async function SendAMessage() {
         }
         ).catch((error) => { alert(`Error${error}`) });
 }
-
-const LogeOutIcon = document.getElementById("LogeOutIcon");
-LogeOutIcon.style.display = "none";
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-         // SingOut  
-        LogeOutIcon.style.display = "inline-block";
-        LogeOutIcon.addEventListener('click', function () {
-            signOut(auth).then(() => {
-                localStorage.clear();
-                window.location.href = '../../Common/Authentication/login.html';
-            }).catch((error) => {
-                alert('Error signing out: ', error);
-            });
-        });
-    }
-})
